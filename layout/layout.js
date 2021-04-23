5ae986d44e530717165b2551f4118c8ce1d9a655
kridtin <kanekoko_kk@outlook.com>
2021-04-23 14:42:04 +0700

    23/04/21-14:42

--- layout/layout.js
+++ layout/layout.js
@@ -15,6 +15,10 @@ export default function layout(props) {
     var dropdown = document.getElementById("farmlist");
     if (dropdown.style.display == "block") {
       dropdown.style.display = "none";
+      for (let index = 0; index < farm.length; index++) {
+        const element = farm[index];
+        document.getElementById(element.name).style.display = "none";
+      }
     } else {
       dropdown.style.display = "block";
     }
@@ -28,71 +32,70 @@ export default function layout(props) {
     }
   }
   return (
-    <div className={styles.container}>
-      <div className={styles.nav}>
-        <div className={styles.user}></div>
-      </div>
-      <div className={styles.sidebar}>
-        <div className={styles.side_icon}>icon</div>
-        <div className={styles.sidenav}>
-          <div id="home" className={styles.menu_item}>
-            <Link href="/">
-              <button onClick={() => navactive("home")}>Home</button>
-            </Link>
-          </div>
-          <div className={styles.menu_item}>
-            <button className="dropdownbtn" onClick={farmlist}>
-              Overview
-            </button>
-          </div>
+    <div>
+      <div className={styles.container}>
+        <div className={styles.nav}>
+          <div className={styles.user}></div>
+        </div>
+        <div className={styles.sidebar}>
+          <div className={styles.side_icon}>icon</div>
+          <div className={styles.sidenav}>
+            <div id="home" className={styles.menu_item}>
+              <Link href="/">
+                <div>Home</div>
+              </Link>
+            </div>
+            <div className={styles.menu_item}>
+              <div className="dropdownbtn" onClick={farmlist}>
+                Overview
+              </div>
+            </div>
 
-          <div id="farmlist" className={styles.dpdwn_container}>
-            {/*map farmname from farmlist json*/}
-            {farm.map((val) => {
-              val.node.map((node) => {
-                return <div></div>;
-              });
-              return (
-                <>
-                  <div>
-                    <button
-                      className={styles.menu_item}
-                      onClick={() => nodelist(val.name)}
-                    >
-                      {val.name}
-                    </button>
-                    <></>
-                  </div>
-                  <div>
-                    <div id={val.name} className={styles.dpdwn_container}>
-                      {val.node.map((nodes) => {
-                        return (
-                          <div
-                            id={val.name + nodes}
-                            className={styles.menu_item}
-                          >
-                            <Link href="/">
-                              <a>{nodes}</a>
-                            </Link>
-                          </div>
-                        );
-                      })}
+            <div id="farmlist" className={styles.dpdwn_container}>
+              {/*map farmname from farmlist json*/}
+              {farm.map((val) => {
+                return (
+                  <>
+                    <div>
+                      <div
+                        className={styles.dropdown_item_1rem}
+                        onClick={() => nodelist(val.name)}
+                      >
+                        {val.name}
+                      </div>
+                      <></>
                     </div>
-                  </div>
-                </>
-              );
-            })}
-          </div>
-          <div className={styles.menu_item}>
-            <button href="/">
-              <a>Organization</a>
-            </button>
+                    <div>
+                      <div id={val.name} className={styles.dpdwn_container}>
+                        {val.node.map((nodes) => {
+                          return (
+                            <div
+                              id={val.name + nodes}
+                              className={styles.dropdown_item_2rem}
+                            >
+                              <Link href="/">
+                                <a>{nodes}</a>
+                              </Link>
+                            </div>
+                          );
+                        })}
+                      </div>
+                    </div>
+                  </>
+                );
+              })}
+            </div>
+            <div className={styles.menu_item}>
+              <div href="/">
+                <a>Organization</a>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-      <div className={styles.content}>{props.children}</div>
-      <div className={styles.footer}>
-        Copyright © 2021-2025 Create by KITFORWARD
+        <div className={styles.content}>{props.children}</div>
+        <div className={styles.footer}>
+          Copyright © 2021-2025 Create by KITFORWARD
+        </div>
       </div>
     </div>
   );
